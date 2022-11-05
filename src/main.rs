744b5cc923ac413b2cb470bbe542e53891e448d9
erwan.vivien <erwan.vivien@epita.fr>
2022-11-05 14:28:45 +0100

    feat: added config & can use it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Config {
+    servers: u16,
+    election_timeout: (f32, f32),
+}
+
+const CONFIG_STR: &'static str = include_str!("../config/config.ron");
+
 fn main() {
-    println!("Hello World!");
+    let config: Config = ron::from_str(CONFIG_STR).expect("");
 }
