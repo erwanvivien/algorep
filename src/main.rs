9438280ae660aa6db7fbbc6cc4521801e7d11d7a
erwan.vivien <erwan.vivien@epita.fr>
2022-11-05 15:26:11 +0100

    wip: first version of usable channels

--- src/main.rs
+++ src/main.rs
@@ -33,8 +33,56 @@ struct Config {
     election_timeout: (ConfigTime, ConfigTime),
 }
 
+type ServerId = usize;
+
+#[derive(Debug)]
+enum MessageContent<'a> {
+    Vote(ServerId),
+    Data(&'a str),
+}
+
+#[derive(Debug)]
+struct Message<'a> {
+    content: MessageContent<'a>,
+    to: ServerId,
+}
+
 const CONFIG_STR: &'static str = include_str!("../config/config.ron");
 
 fn main() {
     let config: Config = ron::from_str(CONFIG_STR).expect("");
+
+    let Config {
+        servers,
+        election_timeout,
+    } = config;
+    let (tx, rx): (Sender<Message>, Receiver<Message>) = mpsc::channel();
+    let mut threads = Vec::with_capacity(servers);
+
+    for id in 0..servers {
+        // The sender endpoint can be copied
+        let thread_tx = tx.clone();
+
+        // Each thread will send its id via the channel
+        let child = thread::spawn(move || {
+            // The thread takes ownership over `thread_tx`
+            // Each thread queues a message in the channel
+            let message = Message {
+                content: MessageContent::Vote(0),
+                to: (id + 1) % servers,
+            };
+            thread_tx.send(message).unwrap();
+        });
+
+        threads.push(child);
+    }
+
+    // Here, all the messages are collected
+    loop {
+        let content = rx.recv();
+        match content {
+            Ok(data) => println!("{:?}", data),
+            Err(RecvError) => (),
+        }
+    }
 }
